17846c4fae53d9e177f72aa841feda2c5ff8f3e7
sathsara sithum <99238978+IT21239298@users.noreply.github.com>
2025-05-27 23:36:38 +0530

    init flutter

---
+++ flutter_application_1/lib/pages/expences.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Expences extends StatefulWidget {
+  const Expences({super.key});
+
+  @override
+  State<Expences> createState() => _ExpencesState();
+}
+
+class _ExpencesState extends State<Expences> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expence master"),
+        backgroundColor: const Color.fromARGB(255, 61, 101, 221),
+        elevation: 0,
+        actions: [
+          Container(
+            color: Colors.yellow,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+                color: Colors.black,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
